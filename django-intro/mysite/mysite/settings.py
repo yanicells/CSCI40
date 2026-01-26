1d76487939dd24e18cd5ac33f04144ea4a8b5829
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-01-26 08:12:24 +0800

    make init django work

--- django-intro/mysite/mysite/settings.py
+++ django-intro/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qgl*g37v9tj$ba=_udv4d&v8b^a^(#z&ra$miikc!acykn$9ld
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
