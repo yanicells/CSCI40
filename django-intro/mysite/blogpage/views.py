3d4e89ba194470c6b16a02194eb9fa2a4ae98d24
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-01-29 08:49:31 +0800

    checkpoint: using inheritance

--- django-intro/mysite/blogpage/views.py
+++ django-intro/mysite/blogpage/views.py
@@ -16,6 +16,35 @@ def task_list(request):
     except requests.RequestException as e:
         return HttpResponse(f"Error fetching data: {e}", status=500)
 
+    url = "https://api.kanye.rest/"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        kanye_data = response.json()
+        data['kanye_quote'] = kanye_data
+
+    except requests.RequestException as e:
+        return HttpResponse(f"Error fetching Kanye quote: {e}", status=500)
+    
+    url = "https://evilinsult.com/generate_insult.php?lang=en&type=json"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        insult_data = response.json()
+        data['insult'] = insult_data
+    except requests.RequestException as e:
+        return HttpResponse(f"Error fetching insult: {e}", status=500)
+
+    url = "https://icanhazdadjoke.com/"
+    try:
+        headers = {'Accept': 'application/json'}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        joke_data = response.json()
+        data['joke'] = joke_data
+    except requests.RequestException as e:
+        return HttpResponse(f"Error fetching joke: {e}", status=500)
+
     ctx={
         "tasks": [
             "task 1",
@@ -26,4 +55,4 @@ def task_list(request):
         "data": data
     }
 
-    return render(request, "task_list.html", ctx)   
+    return render(request, "blogpage/task_list.html", ctx)   
