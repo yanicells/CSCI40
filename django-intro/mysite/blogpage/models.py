52d3c11f5e6ad24e82e4b7fc549aa4450d68ab4c
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-02-12 08:37:44 +0800

    feat: add models

--- django-intro/mysite/blogpage/models.py
+++ django-intro/mysite/blogpage/models.py
@@ -1,3 +1,20 @@
+frp, datetime import datetime
+from djangp.urls import reverse
 from django.db import models
 
-# Create your models here.
+class TaskGroup(models.Model):
+    name = models.CharField(max_length=50)
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    due_date = models.DateTimeField(null=False)
+    taskgroup = models.ForeignKey(TaskGroup, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return '{}: due on {} unit(s)'.format(self.name, self.due_date)
+    def get_absolute_url(self):
+        return reverse('task-detail', args=[str(self.name)])
+
+    @property
+    def is_due(self):
+        return datetime.now() >= self.due_date
